eeadf8129d6b8fd4b09d7f81ef33cd74b34cd6bf
Jakub Suplicki <42224168+jakubsuplicki@users.noreply.github.com>
2025-07-31 14:52:15 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import App from './App.vue'
 import router from './router'
-import '@/assets/styles/main.scss'
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './style.css'
+
 const app = createApp(App)
+
 app.use(createPinia())
-app.use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+app.use(router)
+
+app.mount('#app')
