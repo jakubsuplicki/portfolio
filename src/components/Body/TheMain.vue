f56f181b127536f4e2f7cbadcb6330b3bcf8a38d
Jakub Suplicki <42224168+jakubsuplicki@users.noreply.github.com>
2025-07-31 14:50:11 +0800

    Update TheMain.vue

--- src/components/Body/TheMain.vue
+++ src/components/Body/TheMain.vue
@@ -1,142 +1,55 @@
 <template>
-  <div class="flip">
-    <div
-      :class="{
-        'flip__each--rotate-front': getSelection === 'about'
-      }"
-      class="flip__each flip__each--front"
-      key="landing"
+  <div class="w-full min-h-screen">
+    <transition
+      name="flip"
+      mode="out-in"
+      @enter="onEnter"
+      @leave="onLeave"
     >
-      <the-landing
-        :class="{
-          'flip__each--content-flip': getSelection === 'about'
-        }"
-        class="flip__each--content"
-      />
-      <the-contact />
-    </div>
-    <div
-      :class="{
-        'flip__each--rotate-back': getSelection === 'about'
-      }"
-      class="flip__each flip__each--back"
-      key="about"
-    >
-      <the-about
-        :class="{
-          'flip__each--content-flip': getSelection === 'landing'
-        }"
-        class="flip__each--content"
-      />
-      <the-contact class="flip__contact-about" />
-    </div>
+      <TheLanding v-if="getSelection === 'landing'" key="landing" />
+      <TheAbout v-else-if="getSelection === 'about'" key="about" />
+    </transition>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-import TheLanding from '@/components/Body/MainCard/TheLanding.vue'
-import TheAbout from '@/components/Body/MainCard/TheAbout.vue'
-import TheContact from '@/components/Utils/TheContact.vue'
+<script setup lang="ts">
+import { computed } from 'vue'
+import TheLanding from './MainCard/TheLanding.vue'
+import TheAbout from './MainCard/TheAbout.vue'
 import { useResumeStore } from '@/store/resumeStore'
 
-export default defineComponent({
-  name: 'TheMain',
-  components: {
-    TheLanding,
-    TheAbout,
-    TheContact
-  },
-  computed: {
-    getSelection() {
-      const store = useResumeStore()
-      return store.getSelection
-    }
-  }
-})
+const resumeStore = useResumeStore()
+
+const getSelection = computed(() => resumeStore.getSelection)
+
+const onEnter = (el: Element) => {
+  const element = el as HTMLElement
+  element.style.opacity = '0'
+  
+  setTimeout(() => {
+    element.style.transition = 'opacity 0.5s ease-in-out'
+    element.style.opacity = '1'
+  }, 50)
+}
+
+const onLeave = (el: Element) => {
+  const element = el as HTMLElement
+  element.style.transition = 'opacity 0.3s ease-in-out'
+  element.style.opacity = '0'
+}
 </script>
 
-<style lang="scss" scoped>
-.home {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  overflow: hidden;
+<style scoped>
+.flip-enter-active {
+  transition: opacity 0.5s ease-in-out;
 }
-.flip {
-  position: relative;
-  width: 90vw;
-  height: 48rem;
-  padding-bottom: 2rem;
-  transform: perspective(1000px) rotateY(0deg);
-  margin: 2rem 0;
-  transform-style: preserve-3d;
-  z-index: 1;
-  @include respond(tab-port) {
-    position: relative;
-    display: flex;
-    justify-content: center;
-  }
-  &__each {
-    border-radius: $radius-main;
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: $color-secondary;
-    opacity: 0.9;
-    transform: rotateY(0deg);
-    transform-style: preserve-3d;
-    z-index: 1;
-    &--front,
-    &--back {
-      transition: transform 0.6s cubic-bezier(0.5, 0.3, 0.3, 1);
-      backface-visibility: hidden;
-      overflow: hidden;
-      position: absolute;
-      top: 0;
-      @include respond(tab-port) {
-        position: relative;
-        max-width: 30rem;
-      }
-    }
-    &--front {
-      transform: rotateY(0deg);
-      z-index: 1;
-      @include respond(tab-port) {
-        transform: rotateY(10deg);
-      }
-    }
-    &--back {
-      transform: rotateY(180deg);
-      z-index: 1;
-      @include respond(tab-port) {
-        transform: rotateY(-10deg);
-      }
-    }
-    &--rotate-front {
-      transform: rotateY(-180deg);
-    }
-    &--rotate-back {
-      transform: rotateY(0deg);
-    }
-    &--content {
-      position: relative;
-      transition: transform 0.6s cubic-bezier(0.5, 0.4, 0.4, 1);
-      transform: translateX(0rem) scale(1);
-      z-index: 2;
-    }
-    &--content-flip {
-      transform: translateX(-20rem) scale(0.85);
-      @include respond(tab-port) {
-        transform: none;
-      }
-    }
-  }
-  &__contact-about {
-    @include respond(tab-port) {
-      display: none;
-    }
-  }
+
+.flip-leave-active {
+  transition: opacity 0.3s ease-in-out;
+}
+
+.flip-enter-from,
+.flip-leave-to {
+  opacity: 0;
 }
 </style>
